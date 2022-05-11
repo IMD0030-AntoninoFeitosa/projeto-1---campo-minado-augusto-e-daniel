7396354be7472b05733c0b96b0fa08dd7123377c
DanieLuan <danielluanlourencol@gmail.com>
2022-05-11 16:09:40 -0300

    rename .h to .hpp

---
+++ src/mapa.hpp
@@ -0,0 +1,70 @@
+#ifndef mapa
+#define mapa
+
+#include <vector>
+#include "celula.hpp"
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+
+class Mapa{
+    private:
+
+    int qtdBombas;
+
+    std::vector<std::vector<Celula>> celulas;
+
+    int sizeY;
+    int sizeX;
+
+    int maxBombas;
+
+    int qntRevelados;
+
+    bool perdeu;
+
+    public:
+
+    Mapa();
+
+    int getBombas();
+
+    void criarMapa(std::string diff);
+
+    std::string mostrarMapa();
+
+    void addCelula(std::vector<Celula> linha);
+
+    bool revelarCelula(int i, int j);
+
+    std::string teste();
+
+    bool validar(int y, int x);
+
+    void revelaTeste(int i, int j);
+    
+    Celula getCelula(int x, int y){
+        return celulas[y][x];
+    }
+
+    int getQntRevelados();
+
+    int getSizeY(){
+        return sizeY;
+    }
+
+    int getSizeX(){
+        return sizeX;
+    }
+
+    void botarBandeira(int x, int y);
+
+    void revelarNumero(int x, int y);
+
+    bool getPerdeu();
+
+    void setPerdeu(bool ent);
+};
+
+#endif
